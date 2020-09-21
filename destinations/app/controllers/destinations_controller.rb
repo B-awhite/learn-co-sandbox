6bd4ed22c599ecc590d7fa1b5ef588592f18aab7
Bria White <bria.awhite@gmail.com>
2020-09-21 01:33:29 +0000

    CRU

--- destinations/app/controllers/destinations_controller.rb
+++ destinations/app/controllers/destinations_controller.rb
@@ -5,9 +5,29 @@ class DestinationsController < ApplicationController
     erb :"/destinations/index"
   end 
   
+  get '/destinations/new' do 
+    erb :"/destinations/new"
+  end
+  
   get '/destinations/:id' do 
     @destination = Destination.find_by_id(params[:id])
     erb :"/destinations/show"
+  end 
+  
+  post '/destinations' do 
+    destination = Destination.create(params[:destination])
+    redirect "/destinations/#{destination.id}"
+  end 
+  
+  get "/destinations/:id/edit" do 
+    @destination = Destination.find_by(id:params[:id])
+    erb :"/destinations/edit"
+  end 
+  
+  patch "/destinations/:id" do
+    destination = Destination.find_by(id:params[:id])
+    destination.update(params[:destination])
+    redirect "/destinations/#{destination.id}"
   end 
   
 end 
